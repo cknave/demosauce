ef0b9ab3673604ad8da5cdc6166bee608cc01edf
gir <gir@vs3091157.vserver.de>
2011-10-26 15:08:07 +0200

    added simple-sockulf for testing

---
+++ contrib/simple-sockulf.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+# this is a dumbed down version of sockulf to show how to feed demosauce
+# with songs and metadata. command_nextsong is where all the magic happens
+import socket
+import os
+import random
+
+# a very primitive dj
+class djDerp(object):
+    def __init__(self, root):
+        self.pos = 0
+        print 'generating playlist ... ',
+        self.playlist = self.crawlldir(root)
+        random.shuffle(self.playlist)
+        print len(self.playlist), 'files'
+        if len(self.playlist) == 0:
+            print 'directory is empty!'
+            exit(1)
+
+    def crawldir(self, root):
+        list = []
+        for dir, dirs, files in os.walk(root):
+            for file in files:
+                if file.lower().endswith(".mp3"):
+                    list.append(os.path.join(dir, file)
+        return list
+
+    # returns filename, title, artist, gain
+    def nextsong(self):
+        self.pos += 1
+        if self.pos >= len(self.playlist):
+            randon.suffle(self.playlist)
+            self.pos = 0
+        file = self.palylist[self.pos]
+        return file, os.path.basename(file), '', 0.0
+
+# handles communication with demosauce
+class pyWhisperer(object):
+    def __init__(self, dj, host, port, timeout):
+        self.COMMANDS = {
+            'NEXTSONG': self.command_nextsong,
+        }
+        self.dj = dj
+        self.host = host
+        self.port = port
+        self.encode = self.encode_newline
+        self.running = True
+        self.timeout = timeout
+        self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listener.bind((self.host, self.port))
+        self.listener.settimeout(timeout)
+
+    def listen(self):
+        while self.running:
+            self.listener.listen(1)
+            self.conn, self.addr = self.listener.accept()
+            data = self.conn.recv(1024)
+            i = 0
+            if data:
+                data = data.strip()
+                if data in self.COMMANDS.keys():
+                    result = self.COMMANDS[data]()
+                    while i < len(result):
+                        i = i + self.conn.send(result)
+            self.conn.close()
+        self.listener.close()
+
+    def command_nextsong(self):
+        path, artist, title, gain = self.dj.nextsong()
+        data = {
+            'path': path
+            'artist': artist
+            'title': title
+            'gain': gain
+        }
+        return self.encode(data)
+
+    def encode(self, data):
+        l = []
+        for k, v in data.items():
+            l.append("%s=%s" % (k, v))
+        return "\n".join(l)
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    usage = "usageL %prog [options] path"
+    parser = OptionParser(usage)
+    parser.add_option("-p", "--port", dest="port", default="32167", help = "Which port to listen to")
+    parser.add_option("-i", "--ip", dest="ip", default="127.0.0.1", help="What IP address to bind to")
+
+    (options, args) = parser.parse_args()
+    if len(args) != 1:
+        parser.error("you must specify a path")
+
+    PATH = options.path
+    HOST = options.ip
+    PORT = int(options.port)
+    TIMEOUT = None
+
+    server = pyWhisperer(djDerp(args[0]), HOST, PORT, TIMEOUT)
+    server.listen()
+

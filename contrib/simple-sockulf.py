d0f03ff7d1c7f9485885782f6b195f7f33d72054
foo <bug@me.not>
2011-10-29 00:55:17 +0200

    added license to simple sockulf

--- contrib/simple-sockulf.py
+++ contrib/simple-sockulf.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 # this is a dumbed down version of sockulf to show how to feed demosauce
 # with songs and metadata. command_nextsong is where all the magic happens
+
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# 'maep' on ircnet wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return 
+# ----------------------------------------------------------------------------
+
 import socket
 import os
 import random

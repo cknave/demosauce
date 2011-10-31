9fb4970ee86fc4ba393692c88bbc09604e43f61d
foo <bug@me.not>
2011-11-01 00:29:34 +0100

    tab tab ab

--- ffmpeg/build.sh
+++ ffmpeg/build.sh
@@ -9,10 +9,10 @@ flags_configure="--disable-debug --enable-static --enable-gpl --enable-nonfree -
 if test -f "$source_tar" -a -f "libavcodec.a"; then exit 0; fi
 
 if test ! -f "$source_tar"; then
-        rm -rf *.a
-	echo "attempting to download $source_url"
-	wget "$source_url"
-	if test $? -ne 0; then exit 1; fi
+    rm -rf *.a
+    echo "attempting to download $source_url"
+    wget "$source_url"
+    if test $? -ne 0; then exit 1; fi
 fi
 
 tar -jxf "$source_tar"

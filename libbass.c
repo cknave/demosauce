6d777537cd55b36788506e696533dfa78de37716
foo <bug@me.not>
2011-11-01 21:02:43 +0100

    added licenseplate

--- libbass.c
+++ libbass.c
@@ -1,6 +1,15 @@
-/* this file handles dynamic linking to libbass.so
-* it was nessessary because the system's dynamic 
-* linking isn't flexible enioght
+/*
+*   demosauce - fancy icecast source client
+*
+*   this source is published under the GPLv3 license.
+*   http://www.gnu.org/licenses/gpl.txt
+*   also, this is beerware! you are strongly encouraged to invite the
+*   authors of this software to a beer when you happen to meet them.
+*   copyright MMXI by maep
+*
+*   this file handles dynamic linking to libbass.so
+*   it was nessessary because the system's dynamic 
+*   linking isn't flexible enough
 */
 
 #ifdef __linux__
